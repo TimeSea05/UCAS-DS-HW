63014a2b00b839d109010b5123d17a26ebd02fce
Jason <jason14591@outlook.com>
2022-06-18 23:28:39 +0800

    added function swap()

--- horse-tramp-chessboard/src/weight.c
+++ horse-tramp-chessboard/src/weight.c
@@ -11,6 +11,12 @@ int isOnboard(int x, int y) {
         return 0;
 }
 
+void swap(int* x, int* y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 //看看每个点的八方向中有几个方向走一步后还在棋盘上，并以此作为权值
 void weight() {
     for (int i = 0; i < BOARDSIZE; i++) {
@@ -28,7 +34,6 @@ void weight() {
 void weight_sort() {
     int x1, x2, y1, y2, d1, d2;
     int p, q;
-    int temp;
     for (int i = 0; i < BOARDSIZE; i++) {
         for (int j = 0; j < BOARDSIZE; j++) {
             for (p = 0; p < BOARDSIZE; p++) {
@@ -44,9 +49,7 @@ void weight_sort() {
                     y2 = j + HTry[d2].y;
 
                     if ((!isOnboard(x1, y1) && isOnboard(x2, y2)) || (isOnboard(x1, y1) && isOnboard(x2, y2) && Weight[x1][y1] > Weight[x2][y2])) {
-                        temp = SqWeight[i][j][p];
-                        SqWeight[i][j][p] = SqWeight[i][j][q];
-                        SqWeight[i][j][q] = temp;
+                        swap(&SqWeight[i][j][p], &SqWeight[i][j][q]);
                     }
                 }
             }
